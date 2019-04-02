708e86a80001100a6f4a7510e784d604ee27ec55
matt thompson <mattt2210@gmail.com>
2019-04-02 01:02:10 +0000

    menu in progress

---
+++ Assessment.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main()
+{
+ //menu selection options
+ printf("Enter 1-6 to carry out one of the following operations:\n");
+ printf("(1)  Encrypt message with roatation cipher\n");
+ printf("(2)  Decrypt rotation cuipher with given rotation\n");
+ printf("(3)  Encrypt substitution cipher with given alphabet substitution\n");
+ printf("(4)  Decript substitution cipher with given alphabet substitution\n");
+ printf("(5)  Decrypt rotation cipher with unknown rotation\n");
+ printf("(6)  Decrypt substitution cipher with unknown alphabet substitution\n");
+ 
+ //menu interation s=selected number
+ int s;
+ scanf("%d", &s);
+ switch(s){
+     case 1:
+     
+     break;
+     
+     case 2:
+     
+     break;
+     
+     case 3:
+     
+     break;
+     
+     case 4:
+     
+     break;
+     
+     case 5:
+     
+     break;
+     
+     case 6:
+     
+     break;
+ }
+ 
+
+return 0;
+}
